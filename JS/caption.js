f2bfea9cd09bd1025a21a73fb3de04eaf13e06dd
fahdd95 <61171983+fahdd95@users.noreply.github.com>
2020-07-11 15:51:46 +0200

    Big code

--- JS/caption.js
+++ JS/caption.js
@@ -7,15 +7,42 @@ console.log(lasturl);
 
 
 function readxmllist() {
+  // XML Request and store the information in variable xmlcontent 
+  var xmlcontent;
   var caplistreq = new XMLHttpRequest();
   caplistreq.open("GET", "http://video.google.com/timedtext?type=list&v=wJnBTPUQS5A", true);
   caplistreq.onreadystatechange = function () {
      if (caplistreq.readyState == 4 && caplistreq.status == 200)
      {
-          var doc = caplistreq.responseXML;
-          console.log(doc.getElementById("0"));
+          xmlcontent = caplistreq.responseXML;
+          
+          var langlist = xmlcontent.getElementsByTagName('track');
+          var langcount = langlist.length;
+          console.log(langcount);
+          
+          for(var i = 0; i < langcount ; i++){
+          
+          console.log(langlist[i].getAttribute("lang_translated"));
+              
+          select = document.getElementById('sel1');
+          var opt = document.createElement('option');
+          opt.innerHTML = document.createElement('option');
+          opt.value = langlist[i].getAttribute("lang_code");
+          opt.text = langlist[i].getAttribute("lang_translated");
+          select.appendChild(opt);
+              
+              
+          }
+         
+    
 
      }
 };
 caplistreq.send(null);
+ 
+ //Display the information in the Page
+ 
+    
+    
+    
 }
